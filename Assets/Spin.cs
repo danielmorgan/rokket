71d3d0ab808a2bb0b8d7715801baa18f57d4c547
Daniel Morgan <me@danielmorgan.co.uk>
2017-09-12 18:30:44 +0100

    Glow and spin on Earth

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spin : MonoBehaviour {
+    public float speed;
+    
     void Update () {
-        transform.Rotate(Vector3.right * Time.deltaTime * 50);
-        transform.Rotate(Vector3.up * Time.deltaTime * 50);
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
     }
 }
