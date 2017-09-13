27c86f98418d08db6a8e94f6eed64499f9e1e573
Daniel Morgan <me@danielmorgan.co.uk>
2017-09-13 14:27:19 +0100

    WIP line renderer

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(LineRenderer))]
 public class RocketController : MonoBehaviour {
 
     Rigidbody body;
+    LineRenderer line;
     public float thrustAmount = 2500f;
+    bool dragging = false;
 
     void Start () {
         body = GetComponent<Rigidbody>();
+        line = GetComponent<LineRenderer>();
     }
 
     void FixedUpdate () {
-        if (Input.GetMouseButtonDown(0)) {
-            FireEngine();
+        if (Input.GetMouseButton(0)) {
+            // Going back, check vector3 is only going up in world context
+            Vector3 target = Vector3.up * Input.GetAxis("Mouse Y");
+            line.SetPosition(1, target);
+            Debug.Log(Input.GetAxis("Mouse Y"));
         }
     }
 
