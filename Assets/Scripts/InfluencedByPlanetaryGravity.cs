d1bde5c1d740d3d5d880840bece0664430691ba8
Daniel Morgan <me@danielmorgan.co.uk>
2017-09-13 14:03:55 +0100

    Move gravity to separate script

---
+++ Assets/Scripts/InfluencedByPlanetaryGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InfluencedByPlanetaryGravity : MonoBehaviour {
+
+    Rigidbody body;
+    public GameObject planet;
+    public float gravity = 9.807f;
+    float planetMass;
+
+    void Start () {
+        body = GetComponent<Rigidbody>();
+        planetMass = planet.GetComponent<Rigidbody>().mass;
+    }
+
+	void FixedUpdate () {
+        Vector3 target = planet.transform.position - transform.position;
+        float currentInverseSquareMagnitude = (planet.transform.position - transform.position).sqrMagnitude;
+        float gravityAtCurrentRange = (planetMass * gravity) / currentInverseSquareMagnitude;
+        float magnitudeToApply = body.mass * gravityAtCurrentRange;
+
+        body.AddForce(target.normalized * magnitudeToApply);
+	}
+}
--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -6,29 +6,16 @@ using UnityEngine;
 public class RocketController : MonoBehaviour {
 
     Rigidbody body;
-    public GameObject planet;
-    public float thrustAmount = 2000f;
-    public float gravityFactor = 1f;
+    public float thrustAmount = 2500f;
 
     void Start () {
         body = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate () {
-        ApplyGravity();
-
         if (Input.GetMouseButtonDown(0)) {
             FireEngine();
         }
-    }
-
-    void ApplyGravity () {
-        Vector3 target = planet.transform.position - transform.position;
-        float currentInverseSquareMagnitude = (planet.transform.position - transform.position).sqrMagnitude;
-        float gravityAtCurrentRange = (planet.GetComponent<Rigidbody>().mass * gravityFactor) / currentInverseSquareMagnitude;
-        float magnitudeToApply = body.mass * gravityAtCurrentRange;
-
-        body.AddForce(target.normalized * magnitudeToApply);
     }
 
     void FireEngine () {
