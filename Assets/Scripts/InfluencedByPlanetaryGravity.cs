fa3447b06aa5cf1583c100601cb0bf86ec830130
Daniel Morgan <me@danielmorgan.co.uk>
2017-09-16 19:04:27 +0100

    First attempt at showing trajectory

--- Assets/Scripts/InfluencedByPlanetaryGravity.cs
+++ Assets/Scripts/InfluencedByPlanetaryGravity.cs
@@ -9,6 +9,7 @@ public class InfluencedByPlanetaryGravity : MonoBehaviour {
     public GameObject planet;
     public float gravitationalConstant = 9.807f;
     float planetMass;
+    public float gravity;
 
     void Start () {
         body = GetComponent<Rigidbody>();
@@ -19,8 +20,8 @@ public class InfluencedByPlanetaryGravity : MonoBehaviour {
         Vector3 separation = planet.transform.position - transform.position;
         float magnitude = Mathf.Clamp(separation.sqrMagnitude, 0f, 750f);
         float gravityAtCurrentRange = (planetMass * gravitationalConstant) / magnitude;
-        float magnitudeToApply = body.mass * gravityAtCurrentRange;
+        gravity = body.mass * gravityAtCurrentRange;
 
-        body.AddForce(separation.normalized * magnitudeToApply);
+        body.AddForce(separation.normalized * gravity);
 	}
 }
--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(InfluencedByPlanetaryGravity))]
 public class RocketController : MonoBehaviour {
 
     Rigidbody body;
+    LineRenderer line;
+    InfluencedByPlanetaryGravity g;
 
-    public LineRenderer line;
     float lineOffsetY;
     Vector3 lineEnd;
 
     public float maxThrust = 100f;
-    public float thrustSetRate = 100f;
+    public float thrustSetRate = 50f;
     public float thrustMultiplier = 100f;
     float thrust = 0f;
 
     void Start () {
         body = GetComponent<Rigidbody>();
         line = GetComponentInChildren<LineRenderer>();
+        g = GetComponent<InfluencedByPlanetaryGravity>();
+
         lineOffsetY = line.GetPosition(0).y;
     }
 
@@ -31,12 +34,35 @@ public class RocketController : MonoBehaviour {
             thrust = 0f;
         }
 
-        lineEnd = line.GetPosition(1);
-        lineEnd.y = lineOffsetY + (thrust * 0.1f);
-        line.SetPosition(1, lineEnd);
+        int sampleCount = Mathf.Clamp((int) (thrust), 1, 200);
+        line.positionCount = sampleCount;
+        Vector3[] samples = new Vector3[sampleCount];
+        samples[0] = line.GetPosition(0);
+        for (int x = 1; x < sampleCount; x++) {
+            float y = HeightAtX(x);
+            //Debug.Log(x + "," + y);
+            samples[x] = new Vector3(y / 5, x + lineOffsetY, 0f);
+        }
+        line.SetPositions(samples);
+
+        //lineEnd = line.GetPosition(1);
+        //lineEnd.y = lineOffsetY + (thrust * 0.1f);
+        //line.SetPosition(1, lineEnd);
     }
 
     void FireEngine (float thrust) {
         body.AddForce(transform.forward * thrust * thrustMultiplier);
+    }
+
+    float HeightAtX (int x) {
+        float a = transform.rotation.x * Mathf.Deg2Rad;
+        Vector3 v = thrust * transform.forward;
+
+        return (x * Mathf.Tan(a)) -
+            (
+                (g.gravity * Mathf.Pow(x, 2))
+                /
+                (2 * Mathf.Pow(v.y * Mathf.Cos(a), 2))
+            );
     }
 }
