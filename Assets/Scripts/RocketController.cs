2ed9d924071420dd11d32d01f174c4f88d0adfe2
Daniel Morgan <me@danielmorgan.co.uk>
2017-09-17 02:31:58 +0100

    Render trajectory arc (for a flat surface)

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -2,67 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(InfluencedByPlanetaryGravity))]
+[RequireComponent(typeof(Rigidbody))]
 public class RocketController : MonoBehaviour {
 
     Rigidbody body;
     LineRenderer line;
-    InfluencedByPlanetaryGravity g;
-
     float lineOffsetY;
-    Vector3 lineEnd;
 
-    public float maxThrust = 100f;
-    public float thrustSetRate = 50f;
-    public float thrustMultiplier = 100f;
-    float thrust = 0f;
+	public float thrustMultiplier = 100f;
+	public float thrust = 10f;
+	public float angle = 45f;
+	public int resolution = 20;
 
-    void Start () {
+	float a; // angle in radians
+	float g; // magnitude of gravity (9.82)
+
+    void Awake () {
         body = GetComponent<Rigidbody>();
         line = GetComponentInChildren<LineRenderer>();
-        g = GetComponent<InfluencedByPlanetaryGravity>();
-
         lineOffsetY = line.GetPosition(0).y;
+		g = Mathf.Abs(Physics.gravity.y);
     }
+
+	void OnValidate () {
+		thrust = Mathf.Clamp(thrust, 1, 100);
+		angle = Mathf.Clamp(angle, -90, 90);
+		resolution = Mathf.Clamp(resolution, 0, 50);
+
+		if (line != null) {
+			RenderTrajectoryArc();
+		}
+	}
+
+	void Start () {
+		RenderTrajectoryArc();
+	}
+
+	void RenderTrajectoryArc () {
+		line.positionCount = resolution + 1;
+		line.SetPositions(CalculateArcArray());
+	}
+
+	Vector3[] CalculateArcArray () {
+		Vector3[] arcArray = new Vector3[resolution + 1];
+		float maxDistance = (thrust * thrust * Mathf.Sin(2 * a)) / g;
+
+		for (int i = 0; i < resolution + 1; i++) {
+			float t = (float)i / (float)resolution;
+			arcArray[i] = SampleArc(t, maxDistance);
+		}
+
+		return arcArray;
+	}
+
+	Vector3 SampleArc (float t, float maxDistance) {
+		float x = t * maxDistance;
+		float y = x * Mathf.Tan(a) - ((g * x * x) / (2 * thrust * thrust * Mathf.Cos(a) * Mathf.Cos(a)));
+
+		return new Vector3(x, y + lineOffsetY);
+	}
 
     void Update () {
-        if (Input.GetMouseButton(0)) {
-            float thrustToApply = thrustSetRate * Time.deltaTime;
-            thrust = Mathf.Clamp(thrust += thrustToApply, 0f, maxThrust);
-        } else {
-            FireEngine(thrust);
-            thrust = 0f;
+		a = Mathf.Deg2Rad * angle;
+
+		RenderTrajectoryArc();
+
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 impulse = new Vector3(Mathf.Cos(a), Mathf.Sin(a)) * thrust;
+			body.AddForce(impulse * thrustMultiplier);
         }
-
-        int sampleCount = Mathf.Clamp((int) (thrust), 1, 200);
-        line.positionCount = sampleCount;
-        Vector3[] samples = new Vector3[sampleCount];
-        samples[0] = line.GetPosition(0);
-        for (int x = 1; x < sampleCount; x++) {
-            float y = HeightAtX(x);
-            //Debug.Log(x + "," + y);
-            samples[x] = new Vector3(y / 5, x + lineOffsetY, 0f);
-        }
-        line.SetPositions(samples);
-
-        //lineEnd = line.GetPosition(1);
-        //lineEnd.y = lineOffsetY + (thrust * 0.1f);
-        //line.SetPosition(1, lineEnd);
-    }
-
-    void FireEngine (float thrust) {
-        body.AddForce(transform.forward * thrust * thrustMultiplier);
-    }
-
-    float HeightAtX (int x) {
-        float a = transform.rotation.x * Mathf.Deg2Rad;
-        Vector3 v = thrust * transform.forward;
-
-        return (x * Mathf.Tan(a)) -
-            (
-                (g.gravity * Mathf.Pow(x, 2))
-                /
-                (2 * Mathf.Pow(v.y * Mathf.Cos(a), 2))
-            );
     }
 }
